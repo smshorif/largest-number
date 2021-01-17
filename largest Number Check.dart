58c1ecbf6e2e3279b597422775a5b56640c0325a
Your Name <smshorif7>
2021-01-17 07:09:54 +0600

    frist commit

---
+++ largest Number Check.dart
@@ -0,0 +1,12 @@
+void main() {
+  int a = 10;
+  int b = 40;
+  int c = 30;
+  if(a>b){
+  print("The Largest Number is: ${a}");
+    }else if(b>c){
+ print("The Largest Number is: ${b}");
+  }else{
+   print("The Largest Number is: ${c}");
+  }
+}
--- main.php
+++
@@ -1,6 +0,0 @@
-<php>
-int a=10;
-int b=20;
-int c=a+b;
-print("j"+c);
-</php>
